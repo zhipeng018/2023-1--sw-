62025c5a7f18e73a3ca130eea5ca825b127b04f8
SUPERHU0620 <97418953+SUPERHU0620@users.noreply.github.com>
2023-05-14 19:47:10 +0900

    换成英语

--- fileExploder/src/git/git.js
+++ fileExploder/src/git/git.js
@@ -1,56 +1,56 @@
 const simpleGit = require('simple-git/promise');
 
-// 定义要操作的 Git 存储库的 URL
+// Define the URL of the Git repository to operate on
 const remote = 'https://github.com/SUPERHU0620/test_01.git';
 
-// 存储文件路径的变量
+// Variable to store the file path
 let filePath = '';
 
-// 克隆存储库
+// Clone the repository
 async function clone() {
-  const git = simpleGit();
-  await git.clone(remote);
+const git = simpleGit();
+await git.clone(remote);
 }
 
-// 添加文件到 Git
+// Add file to Git
 async function add() {
-  if (filePath) {
-    const git = simpleGit();
-    await git.add(filePath);
-  } else {
-    console.log('文件路径未设置');
-  }
+if (filePath) {
+const git = simpleGit();
+await git.add(filePath);
+} else {
+console.log('File path not set');
+}
 }
 
-// 提交更改到 Git
+// Commit changes to Git
 async function commit(message) {
-  const git = simpleGit();
-  await git.commit(message);
+const git = simpleGit();
+await git.commit(message);
 }
 
-// 推送更改到远程 Git 存储库
+// Push changes to the remote Git repository
 async function push() {
-  const git = simpleGit();
-  await git.push(['--force', remote, 'HEAD']);
+const git = simpleGit();
+await git.push(['--force', remote, 'HEAD']);
 }
 
-// 获取 Git 存储库中的最新更改
+// Pull the latest changes from the Git repository
 async function pull() {
-  const git = simpleGit();
-  await git.pull(remote, 'HEAD', { '--allow-unrelated-histories': null });
+const git = simpleGit();
+await git.pull(remote, 'HEAD', { '--allow-unrelated-histories': null });
 }
 
-// 设置文件路径的函数
+// Function to set the file path
 function setFilePath(path) {
-  filePath = path;
+filePath = path;
 }
 
-// 导出方法，以便在其他 JavaScript 文件中使用
+// Export the methods for use in other JavaScript files
 module.exports = {
-  clone,
-  add,
-  commit,
-  push,
-  pull,
-  setFilePath,
+clone,
+add,
+commit,
+push,
+pull,
+setFilePath,
 };
