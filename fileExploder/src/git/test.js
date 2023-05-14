301d270e23762bd346e86f2bb63f10ee94bbdfc0
SUPERHU0620 <97418953+SUPERHU0620@users.noreply.github.com>
2023-05-14 19:20:46 +0900

    Update test.js

--- fileExploder/src/git/test.js
+++ fileExploder/src/git/test.js
@@ -1,30 +1,30 @@
 const git = require('./git.js');
 
-// 调用 clone 函数来克隆 Git 存储库
+// Call the clone function to clone the Git repository
 git.clone()
-  .then(() => {
-    console.log('克隆存储库成功');
-    // 添加文件到 Git
-    return git.add('file.txt');
-  })
-  .then(() => {
-    console.log('添加文件成功');
-    // 提交更改到 Git
-    return git.commit('添加文件');
-  })
-  .then(() => {
-    console.log('提交更改成功');
-    // 推送更改到远程 Git 存储库
-    return git.push();
-  })
-  .then(() => {
-    console.log('推送更改成功');
-    // 拉取远程 Git 存储库的最新更改
-    return git.pull();
-  })
-  .then(() => {
-    console.log('拉取更改成功');
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+.then(() => {
+console.log('Repository cloned successfully');
+// Add file to Git
+return git.add('file.txt');
+})
+.then(() => {
+console.log('File added successfully');
+// Commit changes to Git
+return git.commit('Add file');
+})
+.then(() => {
+console.log('Changes committed successfully');
+// Push changes to remote Git repository
+return git.push();
+})
+.then(() => {
+console.log('Changes pushed successfully');
+// Pull the latest changes from the remote Git repository
+return git.pull();
+})
+.then(() => {
+console.log('Changes pulled successfully');
+})
+.catch((error) => {
+console.error(error);
+});
